b5b16af6d4483435791e8108cda50ffd4146047f
codethi <docencia.thiago@gmail.com>
2023-05-25 01:01:47 -0300

    feat: home & get user

--- index.js
+++ index.js
@@ -1,35 +1,84 @@
 import qs from "query-string";
 import axios from "axios";
+import Cookies from "js-cookie";
+
+function alterPage(type) {
+  if (type === "home") {
+    document.querySelector(".home").classList.remove("d-none");
+    document.querySelector(".signin-page").classList.add("d-none");
+  } else {
+    document.querySelector(".home").classList.add("d-none");
+    document.querySelector(".signin-page").classList.remove("d-none");
+  }
+}
 
 function redirectToGithub() {
-  const GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
+  const GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize";
   const params = {
-    response_type: 'code',
-    scope: 'user public_repo',
+    response_type: "code",
+    scope: "user public_repo",
     client_id: process.env.CLIENT_ID,
     redirect_uri: process.env.REDIRECT_URL,
-  }
+  };
 
   const queryStrings = qs.stringify(params);
   const authorizationUrl = `${GITHUB_AUTH_URL}?${queryStrings}`;
   window.location.href = authorizationUrl;
 }
 
-window.onload = async () => {
-  // button
-  document.querySelector(".signin").addEventListener("click", redirectToGithub);
-
-  // checks if user is returning from github
-/*   const { code } = qs.parseUrl(window.location.href).query;
-  if(code) {
+async function getGithubToken(code) {
+  if (code) {
     try {
-      const response = await axios.post(`${process.env.BACK_END_URL}/login`, { code });
-      const user = response.data;
-      alert("você está logado, meu chapa! dá uma olhada no console!");
-      console.log(user);
+      const response = await axios.post(
+        `${process.env.BACK_END_URL}/auth/signin-github`,
+        {
+          code,
+        }
+      );
+      const { token } = response.data;
+      Cookies.set("tokenGithub", token);
+      alterPage("home");
+      getUserByToken();
     } catch (err) {
-      alert("ops, deu algum xabú");
+      const alert = document.querySelector(".alert-signin");
+      alert.classList.add("alert-danger");
+      alert.innerHTML = "Erro ao entrar com o GitHub, tente novamente!";
       console.log("err", err);
     }
-  } */
+  }
 }
+
+async function getUserByToken() {
+  const token = Cookies.get("tokenGithub");
+  try {
+    const response = await axios.get(
+      `${process.env.BACK_END_URL}/users/github/signin`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const user = response.data;
+    document.querySelector(".welcome").innerHTML = `Olá, ${user.name}`;
+  } catch (err) {
+    const alert = document.querySelector(".alert-home");
+    alert.classList.add("alert-danger");
+    alert.innerHTML = "Erro ao buscar usuário!";
+    console.log("err", err);
+  }
+}
+
+function signout() {
+  Cookies.remove("tokenGithub");
+  Cookies.remove("user");
+  alterPage("signin");
+}
+
+window.onload = () => {
+  document.querySelector(".signin").addEventListener("click", redirectToGithub);
+  document.querySelector(".signout").addEventListener("click", signout);
+
+  const { code } = qs.parseUrl(window.location.href).query;
+  getGithubToken(code);
+};
